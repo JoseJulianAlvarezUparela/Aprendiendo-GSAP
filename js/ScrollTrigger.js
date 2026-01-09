1f7f8fa1ae6b5e0733ff127e118bb7dcbdb73797
Jose Julian Alvarez Uparela <josejulianalvarezu@gmail.com>
2026-01-09 11:33:41 -0500

    Vamos con el scrolltrigger

--- js/ScrollTrigger.js
+++ js/ScrollTrigger.js
@@ -1,13 +1,16 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".Square", {
-    x: 700,
-    duration: 3,
+    x: 1000,    duration: 8,
     scrollTrigger: {
-        trigger: ".Square",
-        start: "top 60%",
-        end: "top 40%",
-        toggleActions: "restart pause resume reset",
+        trigger: ".Square2",
+        start: "top 80%",
+        end: "top 30%",
+        scrub: 4,
+        toggleActions: "restart none none none",
+        
+        pin: ".Square",
+        pinSpacing: true,
 
         // onEnter Onleave 
 
@@ -15,3 +18,56 @@ gsap.to(".Square", {
         // toggleClass: "red"
     }
 })
+
+
+
+
+// gsap.to(".box", {
+//     x: 500,
+//     duration: 2
+// })
+
+// gsap.to(".box", {
+//     y: 200,
+//     duration: 3,
+//     delay: 2
+// })
+
+// gsap.to(".box", {
+//     x: -20,
+//     duration: 3,
+//     delay: 5
+// })
+
+// gsap.to(".box", {
+//     y: -10,
+//     duration: 3,
+//     delay: 12
+// })
+
+
+const tl = gsap.timeline();
+
+tl.to(".box", {duration: 3, x: 400})
+   .to(".box", {duration: 3, y: 400})
+   .to(".box", {duration: 3, x: 0})        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
